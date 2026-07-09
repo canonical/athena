259c56c5c1617adec4e9c24ef25e31dcb1efbc29
Mario <mario10rafael@gmail.com>
2026-07-09 13:01:34 +0300

    configured charmcraft.yaml and wrote basic charm class

--- charm/src/charm.py
+++ charm/src/charm.py
@@ -1,57 +1,29 @@
 #!/usr/bin/env python3
-# Copyright 2025 Ubuntu
+# Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""ExpressJS Charm entrypoint."""
+"""Athena ExpressJS charm entrypoint."""
 
 import logging
 import typing
 
 import ops
-
 import paas_charm.expressjs
-from charms.traefik_k8s.v2.ingress import IngressPerAppReadyEvent, IngressPerAppRequirer, IngressPerAppRevokedEvent
 
 logger = logging.getLogger(__name__)
 
-VERSION = "0.0.1"
-ATHENA_SERVICE_PORT = 8080
 
 class AthenaCharm(paas_charm.expressjs.Charm):
-    """ExpressJS Charm service."""
+    """Athena ExpressJS charm service."""
 
     def __init__(self, *args: typing.Any) -> None:
-        """Initialize the instance.
+        """Initialize the charm.
 
         Args:
-            args: passthrough to CharmBase.
+            args: passthrough to the paas_charm ExpressJS charm base.
         """
         super().__init__(*args)
 
-        self.framework.observe(self.on.start, self._on_start)
-        self.ingress = IngressPerAppRequirer(
-            self,
-            port=ATHENA_SERVICE_PORT,
-            healthcheck_params={"path": "/_status/check"},
-        )
-        self.framework.observe(self.ingress.on.ready, self._on_ingress_ready)
-        self.framework.observe(self.ingress.on.revoked, self._on_ingress_revoked)
-
-    def _on_start(self, event: ops.StartEvent):
-        """Set the workload version when the charm starts.  
-
-        Args:  
-            event: The start event.  
-        """  
-        self.unit.set_workload_version(VERSION)
-
-    def _on_ingress_ready(self, event: IngressPerAppReadyEvent) -> None:
-        """Log the ingress URL published by ingress-configurator."""
-        logger.info("Athena ingress is ready at %s", event.url)
-
-    def _on_ingress_revoked(self, event: IngressPerAppRevokedEvent) -> None:
-        """Log ingress removal for Athena."""
-        logger.info("Athena ingress is no longer available")
 
 if __name__ == "__main__":
     ops.main(AthenaCharm)
