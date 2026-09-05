2f46afb828f2541ff488216a7ed95981201f62f5
Ali Dikici <ali.dikici@canonical.com>
2026-09-05 13:54:35 +0300

    service logging

--- src/runner.ts
+++ src/runner.ts
@@ -1,1 +1,10 @@
-console.log(`hello world`);
+let logCount = 0;
+
+const logInterval = setInterval(() => {
+  logCount += 1;
+  console.log(`hello world ${logCount}`);
+
+  if (logCount === 5) {
+    clearInterval(logInterval);
+  }
+}, 1000);
